19ad7b9b4c45ba84c56773f2ee408768a6204521
Vladar4 <vladar4@gmail.com>
2016-03-15 12:03:20 +0200

    fix: xkbmap.sh comments

--- xkbmap.sh
+++ xkbmap.sh
@@ -4,16 +4,23 @@
 #
 # Requirements: setxkbmap xcape
 
+# grp:alt_shift_toggle        Toggle layout with Alt+Shift
+# grp_led:scroll              Use ScrollLock LED as indicator
+# caps:ctrl_modifier          Use CapsLock as Ctrl key in combos
+# compose:ralt                Use RAlt as Compose key
+# terminate:ctrl_alt_bkspc    Enable Ctrl+Alt+Backspace combo
+# grp:switch us,ru            Layouts
 setxkbmap \
   -option \
-  -option grp:alt_shift_toggle \        # Toggle layout with Alt+Shift
-  -option grp_led:scroll \              # Use ScrollLock LED as indicator
-  -option caps:ctrl_modifier \          # Use CapsLock as Ctrl key in combos
-  -option compose:ralt \                # Use RAlt as Compose key
-  -option terminate:ctrl_alt_bkspc \    # Enable Ctrl+Alt+Backspace combo
+  -option grp:alt_shift_toggle \
+  -option grp_led:scroll \
+  -option caps:ctrl_modifier \
+  -option compose:ralt \
+  -option terminate:ctrl_alt_bkspc \
   -option grp:switch \
-  us,ru                                 # Layouts
+  us,ru
 
-xcape -e 'Caps_Lock=Escape'             # Use CapsLock as Escape key
+# Use CapsLock as Escape key
+xcape -e 'Caps_Lock=Escape'
 
 exit 0
